b0211641d53c7e32c0ea98663915b952be19d82c
jakeHaln <JakeHaln@protonmail.com>
2022-09-20 11:34:51 +0800

    fix: js add div to prouct container

--- Project_1/js/main.js
+++ Project_1/js/main.js
@@ -1,7 +1,7 @@
 const programming = document.querySelector(".programming-lang");
 const foods = document.querySelector(".foods");
 const books = document.querySelector(".books");
-const productBox = document.querySelector(".product-box");
+const productContainer = document.querySelector(".product-container");
 const signUp = document.getElementById("sign-up-btu");
 
 const productCount = 9;
@@ -11,36 +11,42 @@ const showProduct = (e) => {
   if (e === "programming-lang") {
     programming.onclick = () => {
       for (let i = 1; i < productCount; i++) {
-        productBox.innerHTML = `
+        productContainer.innerHTML = `
+                <div class="product-box f-class sp-bet">
                     <img class="pro-img" src="../img/programming/programming_${i}.svg" alt="png1">
                     <span>Name</span>
                     <span>Type <span class="type">programming</span></span>
                     <span class="main-price">price <span class="price">$${pricies[i]}</span></span>
                     <button type="button" class="btu-st f-w poit">Buy</button>
+                </div>
                     `;
       }
     };
   } else if (e === "foods") {
     foods.onclick = () => {
       for (let i = 1; i < productCount; i++) {
-        productBox.innerHTML = `
-                    <img class="pro-img" src="../img/foods/foods_${i}.svg" alt="png${i}">
-                    <span>Name</span>
-                    <span>Type <span class="type">${e}</span></span>
-                    <span class="main-price">price <span class="price">$${pricies[i]}</span></span>
-                    <button type="button" class="btu-st f-w poit">Buy</button>
+        productContainer.innerHTML = `
+                    <div class="product-box f-class sp-bet">
+                      <img class="pro-img" src="../img/foods/foods_${i}.svg" alt="png${i}">
+                      <span>Name</span>
+                      <span>Type <span class="type">${e}</span></span>
+                      <span class="main-price">price <span class="price">$${pricies[i]}</span></span>
+                      <button type="button" class="btu-st f-w poit">Buy</button>
+                    </div>
                     `;
       }
     };
   } else {
     books.onclick = () => {
       for (let i = 1; i < productCount; i++) {
-        productBox.innerHTML = `
+        productContainer.innerHTML = `
+                <div class="product-box f-class sp-bet">
                     <img class="pro-img" src="../img/books/books_${i}.svg" alt="png${i}">
                     <span>Name</span>
                     <span>Type <span class="type">${e}</span></span>
                     <span class="main-price">price <span class="price">$${pricies[i]}</span></span>
                     <button type="button" class="btu-st f-w poit">Buy</button>
+                </div>
                     `;
       }
     };
